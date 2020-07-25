850e6d4a9d97b543b921c4c03da926cf8bf1796a
Tobias Briones <tobiasbrionesdev@gmail.com>
2020-07-24 22:56:18 -0600

    .

---
+++ j.js
@@ -0,0 +1,45 @@
+const fun = () => {
+  const fun = () => {
+    const fun = () => {
+      const fun = () => {
+        const fun = () => {
+          const fun = () => {
+            const fun = () => {
+              const fun = () => {
+                const fun = () => {
+                  const fun = () => {
+                    const fun = () => {
+                      const fun = () => {
+                        const fun = () => {
+                          const fun = () => {
+                            const fun = () => {
+                              const fun = () => {
+                                const fun = () => {
+                                  const fun = () => {
+                                    const fun = () => {
+                                      const fun = () => {
+                                        const dsf = 2;
+                                        let fd = 9;
+                                        let odad = fd * dsf;
+
+                                        fd++;
+                                      };
+                                    };
+                                  };
+                                };
+                              };
+                            };
+                          };
+                        };
+                      };
+                    };
+                  };
+                };
+              };
+            };
+          };
+        };
+      };
+    };
+  };
+};
